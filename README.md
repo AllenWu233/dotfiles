0822756230988bff4f0415ba788ba5160ebe735d
AllenWu233 <allenwu233@163.com>
2024-03-20 21:17:03 +0800

    2024/03/20-21:17_UTC+08:00

---
+++ README.md
@@ -0,0 +1,18 @@
+# Allen's personal dotfiles
+
+## Overview
+
+I use Archlinux + i3wm and the repo is my config.
+There is a list of the packages I have installed: ![pkglist.txt](pkglist.txt)
+
+![](images/screenshot_20240320_204044.png)
+![](images/screenshot_20240320_204050.png)
+
+## Introduction
+
+- WM: ![i3](https://i3wm.org/)
+- Status bar: ![polybar](https://github.com/polybar/polybar)
+- Compositor: ![picom-ftlabs](https://aur.archlinux.org/packages/picom-ftlabs-git)
+
+
+
--- backup.sh
+++ backup.sh
@@ -1,11 +1,15 @@
 #!/bin/env bash
 cd ~/repo/dotfiles
-find ./* | grep -v '\(backup.sh\|configlist.txt\|.git\|.gitignore\)' | xargs rm -rf
+
+find ./* | grep -v '\(images\|README.md\|backup.sh\|configlist.txt\|.git\|.gitignore\)' | xargs rm -rf
 cat ./configlist.txt | while read rows
 do
     cp -r ~/.config/$rows .
     rm -rf ./$rows/.git ./$rows.gitignore
 done
+
+pacman -Qqe > pkglist.txt
+
 git add .
 git commit -m $(date -d @$(date +%s) "+%Y/%m/%d-%H:%M_UTC+08:00")
 git push -f origin main
