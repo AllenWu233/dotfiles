0cd72457a3608245464dcb86971467f64ace4bc2
AllenWu233 <allenwu233@163.com>
2024-05-18 16:49:58 +0800

    2024/05/18-16:49_UTC+08:00

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,16 +10,9 @@ local g = vim.g
 -- opt.fileencoding = "utf-8"
 opt.fileencodings = { "utf-8", "ucs-bom", "gb18030", "gbk", "gb2312", "cp936" }
 
--- line number
--- opt.relativenumber = true
--- opt.number = true
-
 -- indent
 opt.tabstop = 4
 opt.shiftwidth = 4
--- opt.expandtab = true -- tab 2 space
--- opt.autoindent = true
--- opt.smartindent = true
 
 opt.wrap = true
 opt.linebreak = true
@@ -33,38 +26,13 @@ opt.wrapmargin = 2
 opt.scrolloff = 999
 -- opt.sidescrolloff = 8
 
--- opt.mouse:append("a")
-
--- opt.clipboard:append("unnamedplus")
-
--- new window in right and below
--- opt.splitright = true
--- opt.splitbelow = true
-
--- search
--- opt.ignorecase = true
--- opt.smartcase = true
-
--- opt.autoread = true
---
 -- time wait for typing shortcut keys
 -- opt.timeoutlen = 400
---
--- opt.signcolumn = "yes"
 
--- opt.showmode = false
---
 -- opt.colorcolumn = "80"
 
 -- show invisible characters
 -- opt.list = true
 -- opt.listchars = "space:·"
-
--- opt.showtabline = 2
---
--- opt.wildmenu = true
--- opt.pumheight = 10
-
--- g.autoformat = false
 
 -- opt.foldmethod = "manual"
