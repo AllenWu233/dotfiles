af119e33b58a699156e32e8cee82f86e9b8565ad
AllenWu233 <allenwu233@163.com>
2024-09-05 21:02:23 +0800

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,43 +1,91 @@
 #!/bin/env bash
+
 cd /home/Allen/repo/dotfiles || exit
 
-find /home/Allen/repo/dotfiles/* | grep -v '\(images\|README.md\|backup.sh\|configlist.txt\|.git\|.gitignore\)' | xargs rm -rf
+# 找到要删除的文件夹并排除不需要的文件夹
+files_to_exclude=(images README.md backup.sh configlist.txt .git .gitignore)
 
-cp -r /home/Allen/scrips .
-cp /home/Allen/.zshrc .
-cp /home/Allen/.zshenv .
-cp /home/Allen/.zimrc .
-cp /home/Allen/.vimrc .
-cp /home/Allen/.gvimrc .
+find /home/Allen/repo/dotfiles/* -type d |
+    # grep -vE '(images|README.md|backup.sh|configlist.txt|.git|.gitignore)' |
+    grep -vE "${files_to_exclude[@]}" |
+    # xargs rm -rf
+    xargs echo
 
-mkdir newsboat
+# 复制指定的文件
+files_to_copy=(scrips .zshrc .zshenv .zimrc .vimrc .gvimrc)
+for file in "${files_to_copy[@]}"; do
+    cp "/home/Allen/$file" .
+done
+
+# 复制newsboat配置
+mkdir -p newsboat
 cp /home/Allen/.config/newsboat/config ./newsboat
 
-mkdir w3m
+# 复制w3m配置
+mkdir -p w3m
 cp /home/Allen/.w3m/config ./w3m
 cp /home/Allen/.w3m/keymap ./w3m
 
-mkdir firefox
+# 复制firefox配置
+mkdir -p firefox
 cp -r /home/Allen/.config/firefox/chrome/ ./firefox
 
+# 复制configlist.txt中列出的配置文件夹
 while IFS= read -r row; do
     cp -r "/home/Allen/.config/$row" .
     rm -rf "./$row/.git" "./$row/.gitignore"
 done <./configlist.txt
 
-# cat ./configlist.txt | while read rows; do
-#     cp -r /home/Allen/.config/"$rows" .
-#     rm -rf ./"$rows"/.git ./"$rows"/.gitignore
-# done
-
+# 生成已安装软件包列表
 pacman -Qqe >pkglist.txt
 
+# 提交变更到Git仓库
 git add .
-# git commit -m $(date -d @$(date +%s) "+%Y/%m/%d-%H:%M_UTC+08:00")
 git commit -m "backup.sh"
 git push -f origin main
 
+# 运行额外的备份脚本
 /home/Allen/repo/hledger/backup.sh
 
+# 输出备份完成的信息
 date
 printf "Backup finished!\n"
+
+#!/bin/env bash
+# cd /home/Allen/repo/dotfiles || exit
+#
+# find /home/Allen/repo/dotfiles/* | grep -v '\(images\|README.md\|backup.sh\|configlist.txt\|.git\|.gitignore\)' | xargs rm -rf
+#
+# cp -r /home/Allen/scrips .
+# cp /home/Allen/.zshrc .
+# cp /home/Allen/.zshenv .
+# cp /home/Allen/.zimrc .
+# cp /home/Allen/.vimrc .
+# cp /home/Allen/.gvimrc .
+#
+# mkdir newsboat
+# cp /home/Allen/.config/newsboat/config ./newsboat
+#
+# mkdir w3m
+# cp /home/Allen/.w3m/config ./w3m
+# cp /home/Allen/.w3m/keymap ./w3m
+#
+# mkdir firefox
+# cp -r /home/Allen/.config/firefox/chrome/ ./firefox
+#
+# while IFS= read -r row; do
+#     cp -r "/home/Allen/.config/$row" .
+#     rm -rf "./$row/.git" "./$row/.gitignore"
+# done <./configlist.txt
+#
+# pacman -Qqe >pkglist.txt
+#
+# git add .
+# # git commit -m $(date -d @$(date +%s) "+%Y/%m/%d-%H:%M_UTC+08:00")
+# git commit -m "backup.sh"
+# git push -f origin main
+#
+# /home/Allen/repo/hledger/backup.sh
+#
+# date
+# printf "Backup finished!\n"
