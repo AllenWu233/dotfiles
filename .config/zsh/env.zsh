16756ae901bf39b978e5fa7ed2dccfdc93ca9823
AllenWu233 <allenwu233@outlook.com>
2025-11-09 10:00:00 +0800

    backup.sh: 2025-11-09 10:00:00

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -11,7 +11,7 @@ export DOTFILES="$HOME/dotfiles"
 export DCONF="$HOME/dotfiles/.config"
 
 # Clash-verge
-export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
+# export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
 
 
 typeset -U path PATH
