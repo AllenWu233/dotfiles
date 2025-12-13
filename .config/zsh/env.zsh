01fe059fdb41c3ab19afec4c66f3b378846327de
AllenWu233 <allenwu233@outlook.com>
2025-12-13 12:00:00 +0800

    backup.sh: 2025-12-13 12:00:00

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -16,7 +16,7 @@ export DCONF="$HOME/dotfiles/.config"
 # export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
 #
 # hongmo.speed
-# export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
 
 typeset -U path PATH
